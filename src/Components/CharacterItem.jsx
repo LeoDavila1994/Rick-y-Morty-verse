5d562d4732f1590e6b76beab7d53aa81fa231d0d
LeoDavila1994 <liodavila.ld@gmail.com>
2022-07-12 17:33:21 -0500

    modal_design

--- src/Components/CharacterItem.jsx
+++ src/Components/CharacterItem.jsx
@@ -12,10 +12,12 @@ const CharacterItem = ({ character }) => {
             .then(res => setCharCard(res.data))
     }, []);
 
+    console.log(charCard);
+
     return (
         <>
-            <input type="checkbox" id="checked"/>
-            <label className='card' htmlFor="checked">
+            <input type="checkbox" id="check"/>
+            <label className='card' htmlFor="check">
                 <div className='img-cont'>
                     <img src={charCard.image} alt="" />
                 </div>
@@ -29,8 +31,11 @@ const CharacterItem = ({ character }) => {
             <div className='modal'>
                 <div className='modal-inf'>
                     <div className='cross-cont'>
-                        <label htmlFor="checked" className='to-close'><i className="fa-solid fa-xmark"></i></label>
+                        <label htmlFor="check" className='to-close'><i className="fa-solid fa-xmark"></i></label>
                     </div>
+                    <div className='img-modal'><img src={charCard.image} alt="" /></div>
+                    <div><p><strong>Name:</strong> {charCard.name}.</p></div>
+                    <div><strong>Status:</strong> <div className='status'><Status charStatus={charCard.status} /></div>{charCard.status}.</div>
                 </div>
             </div>
         </>
